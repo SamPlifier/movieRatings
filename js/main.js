873b91f74a3b145ddc15555e52a2e7b93ff5392c
Robby Helms <robert.helms1@gmail.com>
2016-10-07 15:48:51 -0400

    Added initial star rating interface

--- js/main.js
+++ js/main.js
@@ -4,40 +4,44 @@
 Define API Key to gain access to the movie database
 */
 var user = {
-  username: 'rdhelms',
-  password: 'Natella7712', //Need to have password here to work
+  // username: 'rdhelms',
+  // password: 'Natella7712', //Need to have password here to work
   apiKey: 'c9ef0bc9da53c3ef0e7990875f210ab9',
-  session: null,
-  validSession: false
+  // session: null,
+  // validSession: false
 };
 
-function createSession() {
-  var settings = {
-    "url": "https://api.themoviedb.org/3/authentication/token/new?api_key=" + encodeURIComponent(user.apiKey),
-    "method": "GET"
-  };
-  $.ajax(settings).done(function (response) {
-    console.log(response);
-    user.requestToken = response.request_token;
-    var settings = {
-      "url": "https://api.themoviedb.org/3/authentication/token/validate_with_login?request_token=" + user.requestToken + "&password=" + user.password + "&username=" + user.username + "&api_key=" + user.apiKey,
-      "method": "GET",
-    };
-    $.ajax(settings).done(function (response) {
-      console.log(response);
-      user.requestToken = response.request_token;
-      var settings = {
-        "url": "https://api.themoviedb.org/3/authentication/session/new?request_token=" + user.requestToken + "&api_key=" + user.apiKey,
-        "method": "GET",
-      };
-      $.ajax(settings).done(function (response) {
-        console.log(response);
-        user.session = response.session_id;
-        validSession = true;
-      });
-    });
-  });
-}
+
+// function createSession() {
+//   var settings = {
+//     "url": "https://api.themoviedb.org/3/authentication/token/new?api_key=" + encodeURIComponent(user.apiKey),
+//     "method": "GET"
+//   };
+//   $.ajax(settings).done(function (response) {
+//     console.log(response);
+//     user.requestToken = response.request_token;
+//     var settings = {
+//       "url": "https://api.themoviedb.org/3/authentication/token/validate_with_login?request_token=" + user.requestToken + "&password=" + user.password + "&username=" + user.username + "&api_key=" + user.apiKey,
+//       "method": "GET",
+//     };
+//     $.ajax(settings).done(function (response) {
+//       console.log(response);
+//       user.requestToken = response.request_token;
+//       var settings = {
+//         "url": "https://api.themoviedb.org/3/authentication/session/new?request_token=" + user.requestToken + "&api_key=" + user.apiKey,
+//         "method": "GET",
+//       };
+//       $.ajax(settings).done(function (response) {
+//         console.log(response);
+//         user.session = response.session_id;
+//         validSession = true;
+//       });
+//     });
+//   });
+// }
+
+// Array to keep track of the
+var ratingStars = [];
 
 /**
  Create a constructor that will build our movie details previews
@@ -58,13 +62,44 @@ function MovieDetails(movieObject) {
       moviePoster: this.info.poster,
       movieTitle: this.info.title,
       yourRating: 5,
-      // overview: this.info.overview,
       averageRating: this.info.rating
+      // overview: this.info.overview
     };
     var html = template(context);
     $('.topTwentyBox').html('');
     $(html).prependTo('.topTwentyBox').fadeIn();
     $('.poster').replaceWith('<img class="poster" src=' + this.info.poster + '>');
+    var $stars = $('<div>').attr('class','stars').css({
+      height: '40px',
+      width: '80%',
+      border: '1px solid black',
+      marginTop: '20px',
+      display: 'flex'
+    });
+    for (var index = 0; index < 10; index++) {
+      var $starLeft = $('<div>').attr({
+          'class': 'leftStar star',
+          'data-id': this.info.movieId,
+          'star-id': index
+        }).css({
+          height: '100%',
+          width: '10%',
+          background: 'gray'
+      }).appendTo($stars);
+      ratingStars.push($starLeft);
+      index++;
+      var $starRight = $('<div>').attr({
+          'class': 'rightStar star',
+          'data-id': this.info.movieId,
+          'star-id': index
+        }).css({
+          height: '100%',
+          width: '10%',
+          background: 'black'
+      }).appendTo($stars);
+      ratingStars.push($starRight);
+    }
+    $('.movieInfo').append($stars);
   };
   this.addToList = function() {
     if (movieObject.poster_path !== null) {
@@ -81,7 +116,7 @@ function MovieDetails(movieObject) {
 /**
  Create a function to search for a specific movie
  */
-function showMovie(movieId) {
+function getMovie(movieId) {
   var settings = {
     "url": "https://api.themoviedb.org/3/movie/"+ encodeURIComponent(movieId) +"?api_key="+ user.apiKey,
     "method": "GET",
@@ -109,6 +144,22 @@ function movieSearch(searchString) {
     });
   });
 }
+
+/**
+ Get top rated movies
+ */
+ function getTopRated() {
+    var settings = {
+      "url": "https://api.themoviedb.org/3/movie/top_rated?api_key=" + user.apiKey,
+      "method": "GET",
+    }
+    $.ajax(settings).done(function (response) {
+      response.results.forEach(function(movie) {
+          var newMovie = new MovieDetails(movie);
+          newMovie.addToList();
+      });
+    });
+ }
 
 /**
  Create a function to search for related movies, sending the results to the movie details constructor
@@ -159,6 +210,7 @@ function deleteRating(movieId) {
   });
 }
 
+// When user submits a search, send the request for the relevant movies
 $('form').submit(function(e) {
   e.preventDefault();
   var searchString = $('#movieSearch').val();
@@ -166,6 +218,7 @@ $('form').submit(function(e) {
   $('#movieSearch').val('');
 });
 
+// When the movies are hovered over, scale them larger. When they are clicked, show the full movie view.
 $('.topTwentyBox').on({
   mouseenter: function(){
     $(this).css({
@@ -178,9 +231,40 @@ $('.topTwentyBox').on({
   },
   click: function() {
     var movieId = $(this).attr('data-id');
-    showMovie(movieId);
+    getMovie(movieId);
   }
 }, '.topTwenty');
 
-$('.topTwentyBox').css('justify-content','flex-start').html('');
-createSession();
+// When the rating stars are hovered over, fill the proper stars.
+$('main').on({
+  mouseenter: function() {
+    var current = $(this).attr('star-id');
+    for (var index = 0; index <= current; index++) {
+      $(this).siblings('.star[star-id=' + index + ']').addBack().css('background','red');
+    }
+  },
+  mouseleave: function() {
+    var current = $(this).attr('star-id');
+    for (var index = 0; index <= current; index++) {
+      $(this).siblings('.star[star-id=' + index + ']').addBack().css('background','white');
+    }
+  },
+  click: function() {
+    console.log("Rated!");
+  }
+}, '.star');
+
+
+// When the top twenty button is clicked, view the 20 top rated movies.
+$('.viewTopTwenty').click(function() {
+  $('.topTwentyBox').html('');
+  getTopRated();
+});
+
+// Initialize by clearing the main area and filling it with the top rated movies.
+$('.container').css({
+  position: 'relative',
+  // top: '100px'
+});
+$('.topTwentyBox').html('');
+getTopRated();
